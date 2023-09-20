95b02399a1e19d5a438465ec8eecfd5cd318f22f
zhxchen17 <zhxchen17@meta.com>
2023-09-19 19:55:38 -0700

    [export] Fix tree spec matching behavior.
    Summary:
    
    Test Plan:
    Internal test.
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- torch/fx/_pytree.py
+++ torch/fx/_pytree.py
@@ -1,14 +1,15 @@
-from typing import Callable, Any, Tuple, List, Dict, Type, NamedTuple
-from torch.utils._pytree import PyTree, TreeSpec, LeafSpec
 from collections import namedtuple
+from typing import Any, Callable, Dict, List, NamedTuple, Tuple, Type, Union
 
-FlattenFuncSpec = Callable[[PyTree, TreeSpec], List]
+from torch.utils._pytree import LeafSpec, PyTree, TreeSpec
+
+FlattenFuncSpec = Callable[[PyTree, TreeSpec], Union[List, Tuple[List, bool]]]
 
 SUPPORTED_NODES: Dict[Type[Any], Any] = {}
 def register_pytree_flatten_spec(typ: Any, flatten_fn_spec: FlattenFuncSpec) -> None:
     SUPPORTED_NODES[typ] = flatten_fn_spec
 
-def tree_flatten_spec(pytree: PyTree, spec: TreeSpec) -> List[Any]:
+def tree_flatten_spec(pytree: PyTree, spec: TreeSpec, check_children_match=False) -> List[Any]:
     if isinstance(spec, LeafSpec):
         return [pytree]
     if spec.type not in SUPPORTED_NODES:
@@ -17,24 +18,28 @@ def tree_flatten_spec(pytree: PyTree, spec: TreeSpec) -> List[Any]:
             "torch.fx._pytree.register_pytree_flatten_spec.  If you have serialized your model, make "
             "sure that any custom pytrees have been registered before loading it.")
     flatten_fn_spec = SUPPORTED_NODES[spec.type]
-    child_pytrees = flatten_fn_spec(pytree, spec)
+    child_pytrees = flatten_fn_spec(pytree, spec, check_children_match)
+    if isinstance(child_pytrees, tuple):
+        child_pytrees, children_match = child_pytrees
+        if check_children_match and not children_match:
+            raise RuntimeError()
     result = []
     for child, child_spec in zip(child_pytrees, spec.children_specs):
         flat = tree_flatten_spec(child, child_spec)
         result += flat
     return result
 
-def _dict_flatten_spec(d: Dict[Any, Any], spec: TreeSpec) -> List[Any]:
-    return [d[k] for k in spec.context]
+def _dict_flatten_spec(d: Dict[Any, Any], spec: TreeSpec) -> Tuple[List[Any], bool]:
+    return [d[k] for k in spec.context], len(d) == len(spec.context)
 
-def _list_flatten_spec(d: List[Any], spec: TreeSpec) -> List[Any]:
-    return [d[i] for i in range(len(spec.children_specs))]
+def _list_flatten_spec(d: List[Any], spec: TreeSpec) -> Tuple[List[Any], bool]:
+    return [d[i] for i in range(len(spec.children_specs))], len(d) == len(spec.children_specs)
 
-def _tuple_flatten_spec(d: Tuple[Any], spec: TreeSpec) -> List[Any]:
-    return [d[i] for i in range(len(spec.children_specs))]
+def _tuple_flatten_spec(d: Tuple[Any], spec: TreeSpec) -> Tuple[List[Any], bool]:
+    return [d[i] for i in range(len(spec.children_specs))], len(d) == len(spec.children_specs)
 
-def _namedtuple_flatten_spec(d: NamedTuple, spec: TreeSpec) -> List[Any]:
-    return [d[i] for i in range(len(spec.children_specs))]
+def _namedtuple_flatten_spec(d: NamedTuple, spec: TreeSpec) -> Tuple[List[Any], bool]:
+    return [d[i] for i in range(len(spec.children_specs))], len(d) == len(spec.children_specs)
 
 register_pytree_flatten_spec(dict, _dict_flatten_spec)
 register_pytree_flatten_spec(list, _list_flatten_spec)
